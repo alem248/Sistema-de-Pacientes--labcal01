52cbfa0e0a9ed1ace3eab7fd1cd521c983e14323
Nikolai Suarez <nikolai.suarez@tecsup.edu.pe>
2026-09-09 16:29:10 -0500

    agrego estructura del modulo pacientes

---
+++ src/test/java/com/alex/Paciente/PacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.Paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
